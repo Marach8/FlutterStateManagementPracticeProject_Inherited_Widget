1c8349500a453acd01b36af5b78305df3fe203e9
Ajah Emmanuel Amarachi <emmanuel.fmrs.marach@gmail.com>
2023-11-29 06:11:11 +0100

    Done with the fix

--- inherited_widget_practice/lib/main.dart
+++ inherited_widget_practice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 
 void main(){
@@ -14,10 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue
+        ),
         useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomepage(),
+      home: FakeApiProvider(
+        api: FakeApi(),
+        child: const MyHomepage()
+      ),
     );
   }
 }
@@ -31,18 +38,66 @@ class MyHomepage extends StatefulWidget {
 }
 
 class _MyHomepageState extends State<MyHomepage> {
+
+  ValueKey textKey = const ValueKey<String?>(null);
   String title = 'Tap the Screen to change';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white60,
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(FakeApiProvider.of(context).api.dateAndTime ?? ''), centerTitle: true),
       body: GestureDetector(
-        onTap: (){},
-        child: Container(
-          color: Colors.white
+        onTap: () async{
+          final dateTime = await FakeApiProvider.of(context).api.getDateAndTime();
+          setState(() {textKey = ValueKey(dateTime);});
+        },
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.white,
+            child: DateAndTime(key : textKey)
+          ),
         )
       )
     );
+  }
+}
+
+
+
+class FakeApiProvider extends InheritedWidget{
+  final FakeApi api; final String uuid;
+  FakeApiProvider({
+    Key? key, required Widget child, required this.api,
+    }): uuid = const Uuid().v4(), super(key: key, child: child);
+  
+  @override
+  bool updateShouldNotify(covariant FakeApiProvider oldWidget) {
+    return uuid != oldWidget.uuid;
+  }
+
+  static of(BuildContext context){
+    return context.dependOnInheritedWidgetOfExactType<FakeApiProvider>();
+  }
+}
+
+
+class FakeApi{
+  String? dateAndTime;
+  Future<String> getDateAndTime(){
+    return Future.delayed(
+      const Duration(seconds: 1), () => DateTime.now().toIso8601String()
+    ).then((result) {dateAndTime = result; return result;});
+  }
+}
+
+
+class DateAndTime extends StatelessWidget {
+  const DateAndTime({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final api = FakeApiProvider.of(context).api;
+    return Text(api.dateAndTime ?? 'Tap on the screen to fetch date and time');
   }
 }
